1a630a0a6e846170e65348273b5b8300598a27f5
IanTorrente <ianpaolotorrente@gmail.com>
2018-09-13 23:13:33 -0700

    just reduced spawner size from 6 to 4. Seems to work for now

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -4,13 +4,13 @@ public class FoodSpawner : MonoBehaviour {
   public Rigidbody2D foodPrefab;
 
   void Update () {
-    //Need to do a check that the player isn't holding an item with that finger
+    //Need to do a check that the player isn't holding an item with that finger (seems like DragRigidBody2D already does this. Check in on it though)
     if (Input.touchCount > 0 && GlobalData.globalData.interactable == true) {
       Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
       if (this.GetComponent<Collider2D>() != null && GetComponent<Collider2D>().OverlapPoint(worldPoint)) {
         Rigidbody2D foodInstance;
-        foodInstance = Instantiate(foodPrefab, GetComponent<Transform>().position, GetComponent<Transform>().rotation);
+        foodInstance = Instantiate(foodPrefab, this.GetComponent<Transform>().position, this.GetComponent<Transform>().rotation);
         foodInstance.transform.localScale = foodInstance.transform.localScale;
         Destroy(GetComponent<Collider2D>());
         GetComponent<SpriteRenderer>().enabled = false;
