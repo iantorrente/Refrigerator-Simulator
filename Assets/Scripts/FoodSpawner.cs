db762ce0394bc360fc1c43cb599a96cc7f31e96c
IanTorrente <ianpaolotorrente@gmail.com>
2018-09-13 20:53:53 -0700

    fixed bug with food spawner and not having a collider and still being able to be touched

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -7,14 +7,13 @@ public class FoodSpawner : MonoBehaviour {
     //Need to do a check that the player isn't holding an item with that finger
     if (Input.touchCount > 0 && GlobalData.globalData.interactable == true) {
       Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-      Vector2 touchPos = new Vector2(worldPoint.x, worldPoint.y);
 
-      if (GetComponent<Collider2D>().OverlapPoint(touchPos)) {
+      if (this.GetComponent<Collider2D>() != null && GetComponent<Collider2D>().OverlapPoint(worldPoint)) {
         Rigidbody2D foodInstance;
         foodInstance = Instantiate(foodPrefab, GetComponent<Transform>().position, GetComponent<Transform>().rotation);
+        foodInstance.gravityScale = 0;
         Destroy(GetComponent<Collider2D>());
         GetComponent<SpriteRenderer>().enabled = false;
-        Debug.Log("Touching foods pawner");
       }
     }
   }
