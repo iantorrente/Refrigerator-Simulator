e37922e8883c9e4fbee3edf0b32287bf86eda7e2
eugenemesina <eugenmesina@git>
2018-09-14 19:48:10 -0700

    Changes: testing sequences for food

--- Assets/Scripts/FoodScoreHandler.cs
+++ Assets/Scripts/FoodScoreHandler.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 //Make this class a generic "FoodHandler" to handle scoring and deleting
 //attach it to the eat zone and delete zone when it's genericified
 public class FoodScoreHandler : MonoBehaviour {
+ 	List<string> foodCorrectSequence = new List<string>{"Apple","Burger","Banana"};
+	List<string> foodPlayerSequence = new List<string>{};
 
 	void OnTriggerEnter2D (Collider2D collider) {
+    string name = collider.gameObject.GetComponent<FoodValue>().foodName;
+	  foodPlayerSequence.Add(name);
+    
     int value = collider.gameObject.GetComponent<FoodValue>().scoreValue;
     Helpers.updateScore(value);
+
+    if(foodCorrectSequence.GetRange(0, foodPlayerSequence.Count-1)==foodPlayerSequence) CheckFoodSequence();
+    else foodPlayerSequence = new List<string>();
     Object.Destroy(collider.gameObject);
+  }
+
+  void CheckFoodSequence(){
+	  if(foodPlayerSequence.SequenceEqual( foodCorrectSequence)) Helpers.updateScore(1000);
+	  
   }
 }
