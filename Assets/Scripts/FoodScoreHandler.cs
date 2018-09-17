1e1192969a13f087d8ffbdf85caf563a4810a798
eugenemesina <eugenmesina@git>
2018-09-16 21:04:36 -0700

    Changes: Allows for multiple combination type support

--- Assets/Scripts/FoodScoreHandler.cs
+++ Assets/Scripts/FoodScoreHandler.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 //Make this class a generic "FoodHandler" to handle scoring and deleting
 //attach it to the eat zone and delete zone when it's genericified
 public class FoodScoreHandler : MonoBehaviour {
- 	List<string> foodCorrectSequence = new List<string>{"Apple","Burger","Banana"};
+    List<List<string>> foodCorrectSequences = new List<List<string>>{
+    new List<string>{"Apple", "Burger", "Banana"}, // classic meal
+    new List<string>{"Cake","Cookie","Banana","Cookie","Cake"}, //desert delight
+    new List<string>{"Burger","Steak","Sushi"}, //japanese
+    new List<string>{"Fish","Sushi"} ,//fishy fishy
+    new List<string>{"Banana","Banana"},
+    new List<string>{"Cookie","Cookie"}
+    };
+
+ 
 	List<string> foodPlayerSequence = new List<string>{};
 
 	void OnTriggerEnter2D (Collider2D collider) {
@@ -17,12 +26,26 @@ public class FoodScoreHandler : MonoBehaviour {
     Helpers.increaseScore(value);
     Helpers.startScoreRun();
 
-    if(foodCorrectSequence.GetRange(0, foodPlayerSequence.Count).SequenceEqual(foodPlayerSequence) )CheckFoodSequence();
-    else foodPlayerSequence = new List<string>(){name};
+    
+    var check = false;
+    foreach(List<string> foodCorrectSequence in foodCorrectSequences){
+      if(foodPlayerSequence.Count <= foodCorrectSequence.Count){
+        if(foodCorrectSequence.GetRange(0, foodPlayerSequence.Count).SequenceEqual(foodPlayerSequence))
+        {
+          CheckFoodSequence(foodCorrectSequence);
+          check = true;
+        }        
+      }
+    }
+    if(!check){
+      foodPlayerSequence = new List<string>(){name};
+    }
     Object.Destroy(collider.gameObject);
   }
 
-  void CheckFoodSequence(){
+  
+
+  void CheckFoodSequence(List<string> foodCorrectSequence){
 	  if(foodPlayerSequence.SequenceEqual( foodCorrectSequence)){ 
       Helpers.increaseScore(1000);  
       foodPlayerSequence = new List<string>();
